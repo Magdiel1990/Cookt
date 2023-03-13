3d49aedfba3b95f36391c5a200db4b8300ccfd5b
Magdiel Castillo <magdielmagdiel1@gmail.com>
2023-03-13 19:25:43 -0400

    login picture name changed

--- login/login.php
+++ login/login.php
@@ -25,7 +25,7 @@ require_once ("../modules/head.php");
         <div class="container-fluid h-custom my-4">
             <div class="row d-flex justify-content-center align-items-center h-100">
                 <div class="col-md-9 col-lg-6 col-xl-5">
-                    <img src="../imgs/loginimgs/Login picture.jfif" class="img-fluid" alt="Sample image">
+                    <img src="../imgs/login/Picture.png" class="img-fluid" alt="Sample image">
                 </div>
                 <div class="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
                     <form action="" method="POST">
