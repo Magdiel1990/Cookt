1596d3d9713237efedcc30cffdbe47dea0d602b5
Magdiel Castillo <magdielmagdiel1@gmail.com>
2023-03-17 09:30:22 -0400

    banner link done

--- modules/nav.php
+++ modules/nav.php
@@ -1,7 +1,7 @@
 <script src="https://kit.fontawesome.com/65a5e79025.js" crossorigin="anonymous"></script>
 <header>
     <div class="banner text-center py-1">
-        <a href="#">Recipes23</a>        
+        <a href="/Cookt/index.php">Recipes23</a>        
     </div>
     <nav class="navbar navbar-expand-lg navbar-dark bg-secondary px-2">
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
