7ab0ad8f1b1d8bf412ad979475261a50eded4a7e
Magdiel Castillo <magdielmagdiel1@gmail.com>
2023-03-18 13:33:37 -0400

    login in only one page

--- login.php
+++ login.php
@@ -7,6 +7,55 @@ session_start();
 
 //Models.
 require_once ("models/models.php");
+
+//Including the database connection.
+require_once ("config/db_Connection.php");
+
+if (isset($_POST['username']) && isset($_POST['password'])) {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    //Verifico los datos del usuario.
+    $sql = "SELECT * FROM users WHERE username = '$username' AND password = '$password';";
+    $result = $conn -> query($sql);
+    $num_rows =  $result -> num_rows;    
+  
+    //Si el usuario existe verifico la contraseña.
+    if ($num_rows > 0) {
+
+        $row = $result -> fetch_assoc();
+       
+        if ($password == $row['password'] && $row['state'] = 1) {
+             //When a new user logs in, the index page is always the first page to load.
+            if($_SESSION['username'] != $row['username']) {
+                unset($_SESSION['lastpage']);
+            }
+
+            if(!isset($_SESSION['lastpage'])){
+                $_SESSION['lastpage'] = "/Cookt/index.php";
+            }
+
+            //Creo la cookie.        
+            session_set_cookie_params(0, "/", $_SERVER["HTTP_HOST"], 0);
+            //Declaro las variables de la sesión.
+            $_SESSION["login"] = "yes";
+            //Calcula la hora y fecha del momento en el que se crea la sesión.
+            $_SESSION["last_access"] = date("Y-n-j H:i:s");
+            $_SESSION['userid'] = $row['userid'];
+            $_SESSION['fullname'] = $row['fullname'];
+            $_SESSION['username'] = $row['username'];
+            $_SESSION['type'] = $row['type'];
+            $_SESSION['email'] = $row['email'];
+            $_SESSION['state'] = $row['state'];       
+
+            header("Location: ". $_SESSION['lastpage']);
+        }
+    } else {
+        $_SESSION['message'] = "¡Usuario o contraseña incorrectos!";
+        $_SESSION['message_alert'] = "danger";
+    }
+} 
+
 ?>
 <!DOCTYPE html>
 <html lang="es" data-lt-installed="true">
@@ -48,7 +97,7 @@ require_once ("models/models.php");
                     <img src="imgs/login/Picture.png" class="img-fluid" alt="Sample image">
                 </div>
                 <div class="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
-                    <form action="/Cookt/user-handler/login-verif.php" method="POST">
+                    <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
                         <!-- Email input -->
                         <div class="form-outline mb-3">
                             <input type="text" id="username" class="form-control form-control-lg" name="username"/>
@@ -78,6 +127,7 @@ require_once ("models/models.php");
     </section>
 </main>
 <?php
+$conn -> close();
 //Footer of the page.
 require_once ("modules/footer.php");
 ?>
