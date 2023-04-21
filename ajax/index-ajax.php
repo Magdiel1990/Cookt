6321827a8729b922b2291931fa963a036b519a00
Magdiel Castillo <magdielmagdiel1@gmail.com>
2023-04-21 09:49:34 -0400

    index fixed

--- ajax/index-ajax.php
+++ ajax/index-ajax.php
@@ -5,7 +5,8 @@
     session_start();
 
     //Models.
-    require_once ("../models/models.php");
+    require_once ("..
+    /models/models.php");
 
     //Including the database connection.
     $conn = DatabaseConnection::dbConnection();
