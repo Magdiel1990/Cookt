52ea3d3bb6db9e1ae5919382844def238830d098
Magdiel Castillo <magdielmagdiel1@gmail.com>
2023-04-22 10:44:10 -0400

    login fixed

--- login.php
+++ login.php
@@ -113,11 +113,11 @@ if (isset($_POST['username']) && isset($_POST['password'])) {
     
     <section class="my-4">
         <div class="container-fluid h-custom my-4">
-            <div class="row d-flex justify-content-center align-items-center h-100">
+            <div class="row d-flex justify-content-center align-items-center">
                 <div class="col-md-9 col-lg-6 col-xl-5">
-                    <img src="imgs/login/Picture.png" class="img-fluid" alt="Sample image">
+                    <img src="imgs/login/Picture.png" class="img-fluid img-thumbnail" alt="Sample image">
                 </div>
-                <div class="col-md-8 col-lg-6 col-xl-4 offset-xl-1 mt-sm-4">
+                <div class="col-md-8 col-lg-6 col-xl-4 offset-xl-1 mt-4">
                     <form action="/cookt/login" method="POST">
                         <!-- Email input -->
                         <div class="form-outline mb-3">
