ce4d8e50656e5099f30ff07d661570036e227daa
Magdiel Castillo <magdielmagdiel1@gmail.com>
2023-04-24 10:41:31 -0400

    nav comment

--- views/partials/nav.php
+++ views/partials/nav.php
@@ -1,11 +1,13 @@
 <header>
     <nav class="navbar navbar-expand-md navbar-dark px-4">
+<!-- Logo and dropdown button-->
         <div class="logo"> 
             <a class="nav-link text-white" href="/"><img src="imgs/logo/logo2.png" alt="Logo"></i></a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>  
         </div>   
+<!-- Nav links -->        
         <div class="collapse navbar-collapse" id="navbarNavDropdown">
             <ul class="navbar-nav">
             <li class="nav-item px-1">
@@ -18,6 +20,7 @@
                 <a class="nav-link dropdown-toggle text-white" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 <i class="fa-solid fa-gears text-white"></i>
                 </a>
+<!-- Not Admin users can't access some of the links-->
                 <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
                     <a <?php if($_SESSION['type'] != 'Admin') { echo "style = 'display : none;'";}?> class="dropdown-item" href="/units">Unidades</a>
                     <a class="dropdown-item" href="/ingredients">Ingredientes</a>
@@ -28,12 +31,13 @@
             </li>            
             </ul>
         </div>
+<!--User identification -->
         <div class="d-flex flex-row">            
             <a class="px-3 text-white"  href="/profile" style="text-decoration: none;" title="Usuario">
             <?php echo $_SESSION['title'] . $_SESSION['firstname'] . " " .  $_SESSION['lastname'];?> 
             <?php
             $target_dir = "imgs/users/";
-
+//User photo
             $files = new Directories($target_dir, $_SESSION["username"]);
             $imgprofileDir = $files -> directoryFiles();
             
@@ -44,7 +48,8 @@
             }
             
             ?>
-            </a>        
+            </a>  
+<!-- Logout button -->                  
             <a class="nav-link text-white logout" href="/logout" title="Salir"> <i class="fa-solid fa-right-from-bracket"></i></a>     
         </div>          
     </nav>
