035b13bb5979ad3b8c3d63296b77bd74468e48ad
Magdiel Castillo <magdielmagdiel1@gmail.com>
2023-04-24 10:47:29 -0400

    category comment

--- views/add-categories.view.php
+++ views/add-categories.view.php
@@ -1,32 +1,34 @@
 <?php
-//Head of the page.
+//Head
 require_once ("views/partials/head.php");
 ?>
 
 <?php
+//Not Admin users can't access
 if($_SESSION['type'] != 'Admin') { 
     require_once ("views/error_pages/404.php");
     exit;
 }
 
-//Navigation panel of the page
+//Nav of the page
 require_once ("views/partials/nav.php");
 ?>
 
 <main class="container p-4">
     <div class="row mt-2 text-center justify-content-center">
     <?php
-//Messages that are shown in the add_units page
+//Messages 
         if(isset($_SESSION['message'])){
         $message = new Messages ($_SESSION['message'], $_SESSION['message_alert']);
         echo $message -> buttonMessage();           
 
-//Unsetting the messages variables so the message fades after refreshing the page.
+//Unsetting the messages
         unset($_SESSION['message_alert'], $_SESSION['message']);
         }
     ?>
+<!--Category form-->
     <h3>Agregar Categorías</h3>
-<!--Form for filtering the database info-->
+
         <form class="mt-3 col-auto"  enctype="multipart/form-data" method="POST" action="/create" autocomplete="on" onsubmit="return validation('add_categories', /[a-zA-Z\t\h]+|(^$)/)">
             
             <div class="input-group mb-3">
@@ -38,12 +40,14 @@ require_once ("views/partials/nav.php");
                 <label class="form-label is-required" for="categoryImage">Foto de la categoría</label>
                 <input type="file" name="categoryImage" accept=".png, .jpeg, .jpg, .gif" class="form-control" id="categoryImage" required>
             </div> 
+
             <div class="mb-3">
                 <input  class="btn btn-success" name="categorySubmit" type="submit" value="Agregar">
             </div>
 
         </form>
     </div>
+<!-- Category list -->
     <div class="table-responsive-sm mt-4">
         <table class="table table-sm">
             <thead>
@@ -63,6 +67,7 @@ require_once ("views/partials/nav.php");
                             $html .= "<td>" . ucfirst($row['category']) . "</td>";
                             $html .= "<td>";
                             $html .= "<div class='btn-group' role='group'>";
+//Delete and edit buttons
                             $html .= "<a href='/delete?categoryname=" . $row['category'] . "' " . "class='btn btn-outline-danger' title='Eliminar'><i class='fa-solid fa-trash'></i></a>";
                             $html .= "<a href='/edit?categoryid=" . $row['categoryid'] . "' " . "class='btn btn-outline-secondary' title='Editar'><i class='fa-solid fa-pen'></i></a>";
                             $html .= "</div>";
@@ -71,6 +76,7 @@ require_once ("views/partials/nav.php");
                             echo $html;
                         }
                     } else {
+//Text when there is no categories                        
                         $html = "<tr>";
                         $html .= "<td colspan='2'>";
                         $html .= "Agrega las categorías...";
@@ -84,7 +90,8 @@ require_once ("views/partials/nav.php");
     </div>
 </main>
 <?php
+//Close db connection
 $conn -> close();
-//Footer of the page.
+//Footer
 require_once ("views/partials/footer.php");
 ?>
