77f8794c3de19f90750ed7bdc1da02ca66ae45d6
Magdiel Castillo <magdielmagdiel1@gmail.com>
2023-05-02 10:52:19 -0400

    ajax comment

--- ajax/index-ajax.php
+++ ajax/index-ajax.php
@@ -1,31 +1,31 @@
 <?php
-    //Reviso el estado de la sesión.
+//Reviso el estado de la sesión.
     session_name("Login");
     
     session_start();
 
     $_SESSION["location"] = "/";
 
-    //Models.
+//Models.
     require_once ("../models/models.php");
 
-    //Including the database connection.
+//Including the database connection.
     $conn = DatabaseConnection::dbConnection();
     
-    //Array of the columns to be querried from the database.
+//Array of the columns to be querried from the database.
     $columns = ["r.recipename", "c.category", "r.cookingtime"];
     
-    //Table to be querried from the database.
+//Table to be querried from the database.
     $table = "recipe r JOIN categories c ON r.categoryid = c.categoryid";
 
-    //If the variable search is set it's received, else it's null. 
+//If the variable search is set it's received, else it's null. 
     $filter = new Filter ($_POST["search"], FILTER_SANITIZE_STRING, $conn);
     $field = $filter -> sanitization(); 
 
-    //Filter where
+//Filter where
     $where = " WHERE username = '" . $_SESSION['username'] . "'";
 
-    //If the variable search isn't null, the query is done with the where.
+//If the variable search isn't null, the query is done with the where.
     if($field != ""){
         $where = "WHERE (";
 
@@ -33,7 +33,7 @@
         for($i = 0; $i < $count; $i++){
             $where .= $columns[$i] . " LIKE '%" . $field . "%' OR ";
         }
-    //The final where delection.
+//The final where delection.
         $where = substr_replace($where, "", -3);
         $where .= "AND username = '" . $_SESSION['username'] . "')";
     }        
@@ -42,14 +42,14 @@
     FROM $table 
     $where ORDER BY rand();";
 
-    //Count of the number of rows of the query
+//Count of the number of rows of the query
     $result = $conn->query($sql);
     $num_rows = $result-> num_rows;
 
     $output = [];
     $output['data'] = '';
 
-    //If there are results for the query , they are shown.
+//If there are results for the query , they are shown.
     if($num_rows > 0) {
         while($row = $result->fetch_assoc()){
             $output['data'] .= "<tr>";            
@@ -65,14 +65,14 @@
             $output['data'] .= "</tr>";
         }
     } else {
-    //Else this message is shown.
+//Else this message is shown.
         $output['data'] .= "<tr>";
         $output['data'] .= "<td colspan = '3'>No hay resultados</td>";
         $output['data'] .= "</tr>";
     }
-    //Json file is encoded and echoed excluding especial characters.
+//Json file is encoded and echoed excluding especial characters.
     echo json_encode($output, JSON_UNESCAPED_UNICODE);
 
-    //Closing the connection.
+//Closing the connection.
     $conn -> close();    
 ?>
