a954bb5e5a1d1a4cbc7a69fb0548370b340e0c7a
Magdiel Castillo <magdielmagdiel1@gmail.com>
2023-05-02 18:51:48 -0400

    reset comment

--- actions/reset.php
+++ actions/reset.php
@@ -1,15 +1,12 @@
 <?php
-
 /************************************************************************************************/
 /******************************************USER RESET CODE***************************************/
 /************************************************************************************************/
 
-//Verifying that the id value comes with data.
 if(isset($_GET['user_id']) && isset($_GET['reset'])) {
-    
-//Getting the name.
-$userId = $_GET['user_id'];
 
+$userId = $_GET['user_id'];
+//User data
 $sql = "SELECT * FROM users WHERE userid = '$userId';";
 $row = $conn -> query($sql) -> fetch_assoc();
 
@@ -22,33 +19,31 @@ $email = $row['email'];
 $state = $row['state'];
 $sex = $row['sex'];
 
+//User image directory
 $target_dir = "imgs/recipes/" . $username;
-
+//Delete user image directory
     if(file_exists($target_dir)) {
         unlink($target_dir);
     }
     
-//Deleting the register with the name received.
+//Deleting the user account
 $sql = "DELETE FROM users WHERE username = '$username';";
 
+//Inserting the user data back
     if($conn -> query($sql)){
         $sql = "INSERT INTO users (userid, username, firstname, lastname, `password`, `type`, email, `state`, sex)
         VALUES ($userId, '$username', '$firstname', '$lastname', '$password', '$type', '$email', $state, '$sex');";
 
         if($conn -> query($sql) === TRUE){
-            //Creation of the message of success deleting the receta.
             $_SESSION['message'] = '¡Usuario reseteado!';
             $_SESSION['message_alert'] = "success";
 
-    //After the receta has been deleted, the page is redirected to the add_units.php.
             header("Location: /user");
 
         } else {
-        //Creation of the message of error deleting the receta.
             $_SESSION['message'] = '¡Error al resetear usuario!';
             $_SESSION['message_alert'] = "danger";
 
-    //The page is redirected to the add_units.php
             header("Location: /user");
         }
     }
