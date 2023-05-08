8f1e90f8b514a1716a0e6c796cfb8676467f2c53
Magdiel Castillo <magdielmagdiel1@gmail.com>
2023-05-08 19:47:55 -0400

    Nav lINKs DONE

--- views/partials/nav.php
+++ views/partials/nav.php
@@ -2,7 +2,7 @@
     <nav class="navbar navbar-expand-md navbar-dark px-4">
 <!-- Logo and dropdown button-->
         <div class="logo"> 
-            <a class="nav-link text-white" href="/"><img src="imgs/logo/logo2.png" alt="Logo" title="Página principal"></i></a>
+            <a class="nav-link text-white" href="<?php echo root;?>/"><img src="<?php echo root;?>imgs/logo/logo2.png" alt="Logo" title="Página principal"></i></a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>  
@@ -11,10 +11,10 @@
         <div class="collapse navbar-collapse" id="navbarNavDropdown">
             <ul class="navbar-nav">
             <li class="nav-item">
-                <a class="nav-link text-white" href="/random" title="Sugerencias">Sugerencias</a>
+                <a class="nav-link text-white" href="<?php echo root;?>/random" title="Sugerencias">Sugerencias</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link text-white" href="/custom" title="Elegir por ingredientes">Elegir por ingredientes</a>
+                <a class="nav-link text-white" href="<?php echo root;?>/custom" title="Elegir por ingredientes">Elegir por ingredientes</a>
             </li>
             <li class="nav-item dropdown"  <?php if($_SESSION['type'] == 'Viewer') { echo "style = 'display : none;'";}?>>
                 <a class="nav-link dropdown-toggle text-white" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -22,20 +22,20 @@
                 </a>
 <!-- Not Admin users can't access some of the links-->
                 <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">                    
-                    <a class="dropdown-item" href="/ingredients" title="Ingredientes">Ingredientes</a>
-                    <a class="dropdown-item" href="/add-recipe" title="Recetas">Recetas</a>
-                    <a <?php if($_SESSION['type'] != 'Admin') { echo "style = 'display : none;'";}?> class="dropdown-item" href="/categories" title="Categorías">Categorías</a>
-                    <a <?php if($_SESSION['type'] != 'Admin') { echo "style = 'display : none;'";}?> class="dropdown-item" href="/user" title="Usuarios">Usuarios</a>
+                    <a class="dropdown-item" href="<?php echo root;?>/ingredients" title="Ingredientes">Ingredientes</a>
+                    <a class="dropdown-item" href="<?php echo root;?>/add-recipe" title="Recetas">Recetas</a>
+                    <a <?php if($_SESSION['type'] != 'Admin') { echo "style = 'display : none;'";}?> class="dropdown-item" href="<?php echo root;?>/categories" title="Categorías">Categorías</a>
+                    <a <?php if($_SESSION['type'] != 'Admin') { echo "style = 'display : none;'";}?> class="dropdown-item" href="<?php echo root;?>/user" title="Usuarios">Usuarios</a>
                 </div>
             </li>            
             </ul>
         </div>
 <!--User identification -->
         <div class="d-flex flex-row">            
-            <a class="px-3 text-white"  href="/profile" style="text-decoration: none;" title="Perfil">
+            <a class="px-3 text-white"  href="<?php echo root;?>/profile" style="text-decoration: none;" title="Perfil">
             <?php echo $_SESSION['title'] . $_SESSION['firstname'] . " " .  $_SESSION['lastname'];?> 
             <?php
-            $target_dir = "imgs/users/";
+            $target_dir = root . "/imgs/users/";
 //User photo
             $files = new Directories($target_dir, $_SESSION["username"]);
             $imgprofileDir = $files -> directoryFiles();
@@ -49,7 +49,7 @@
             ?>
             </a>  
 <!-- Logout button -->                  
-            <a class="nav-link text-white logout" href="/logout" title="Salir"> <i class="fa-solid fa-right-from-bracket"></i></a>     
+            <a class="nav-link text-white logout" href="<?php echo root;?>/logout" title="Salir"> <i class="fa-solid fa-right-from-bracket"></i></a>     
         </div>          
     </nav>
 </header>
